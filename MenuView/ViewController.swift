cb489920aa1548fb5147cf417d439e0700433dba
shoutokobayashi <deepstrap353@gmail.com>
2020-05-29 17:17:24 +0900

    Initial Commit

---
+++ MenuView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MenuView
+//
+//  Created by 小林聖人 on 2020/05/29.
+//  Copyright © 2020 小林聖人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
